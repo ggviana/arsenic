632a8c37650a51ecf75f32392b2c7f781786a889
Guilherme Guimarães <guilherme.viana@dashboard.bi>
2016-05-16 14:45:18 -0300

    Moving reducer logic

---
+++ app/lobby/reducer.js
@@ -0,0 +1,15 @@
+const initial = {
+  rooms: [
+    {
+      id: '9ja8',
+      name: 'Some'
+    }
+  ]
+}
+
+export default (state = initial, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
--- app/reducers.js
+++ app/reducers.js
@@ -1,1 +1,5 @@
-export default {}
+import lobby from 'lobby/reducer'
+
+export default {
+  lobby
+}
